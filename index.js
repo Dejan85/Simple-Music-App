e82cd32d93e3d75d59f583edb54c1721cc3fdea3
unknown <xypnotica@gmail.com>
2019-05-30 02:54:38 +0200

    animation class done

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ class UI {
     return { sound, visual, bubble };
   };
 }
+
+class Animation {
+  static animate = function(index) {
+    const colors = [
+      "#60d394",
+      "#d36060",
+      "#c060d3",
+      "#d3d160",
+      "#606bd3",
+      "#60c2d3"
+    ];
+
+    const visual = UI.dom().visual;
+    const bubble = UI.dom().bubble;
+
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = "jump 1s ease";
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
+  };
+}
